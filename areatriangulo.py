58aef289f1dcecc565d33c35f8bfd91a74e15b75
wera <69810414+natlujan@users.noreply.github.com>
2023-03-02 14:36:26 -0700

    Create areatriangulo.py

---
+++ areatriangulo.py
@@ -0,0 +1,12 @@
+print("calcular el área de un triángulo \n")
+print("ingresa la base del triángulo: ")
+
+base = float(input(())
+
+print("ingresa la altura del triángulo: ")
+
+height = float(input())
+
+area = (base * height)/2
+
+print("el área del triángulo es: ", area)
